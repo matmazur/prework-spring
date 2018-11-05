07ed27f0233cc917708fe95871ae7a8118b55c42
Bingo Bobo <matmazur90@gmail.com>
2018-11-05 13:21:48 +0100

    app class Main worked properly getting creating context based on xml configuration

--- src/main/java/com/matmazur/app/Main.java
+++ src/main/java/com/matmazur/app/Main.java
@@ -1,8 +1,18 @@
 package com.matmazur.app;
 
+import com.matmazur.beans.MessagePrinter;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
 
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans-config.xml");
+
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+
+        printer.getProducer().produce("Stuff, things.");
+
+        context.close();
 
     }
 }
--- src/main/java/com/matmazur/beans/StringMessageProducer.java
+++ src/main/java/com/matmazur/beans/StringMessageProducer.java
@@ -4,6 +4,6 @@ public class StringMessageProducer implements MessageProducer {
 
     @Override
     public void produce(String s) {
-        System.out.println(s);
+        System.out.println("String of the day is : " + s);
     }
 }
