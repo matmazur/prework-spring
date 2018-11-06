f3d593fe85f142e6f46e4f51dd053c48cf001165
Bingo Bobo <matmazur90@gmail.com>
2018-11-06 12:08:05 +0100

    created textfile for test check

--- src/main/java/com/matmazur/beans/decorators/Decorator.java
+++ src/main/java/com/matmazur/beans/decorators/Decorator.java
@@ -2,9 +2,7 @@ package com.matmazur.beans.decorators;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 import static java.lang.annotation.ElementType.*;
 
@@ -16,7 +14,7 @@ public @interface Decorator {
     DecoratorType type();
 
     enum DecoratorType {
-        DUMB, LOWER;
+        DUMB, LOWER
     }
 
 }
