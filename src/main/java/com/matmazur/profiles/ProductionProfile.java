ba0c923229d9212179deed42262881bae9c96a05
Bingo Bobo <matmazur90@gmail.com>
2018-11-06 17:50:26 +0100

    added 2 profiles for dev and production enviroment

---
+++ src/main/java/com/matmazur/profiles/ProductionProfile.java
@@ -0,0 +1,14 @@
+package com.matmazur.profiles;
+
+import org.springframework.context.annotation.Profile;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Profile("production")
+public @interface ProductionProfile {
+}
